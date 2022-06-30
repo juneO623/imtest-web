120a9cfd6be830443ad587900faf4b2c02d2153d
username <81845513+june3011@users.noreply.github.com>
2022-06-30 23:06:39 +0900

    Update : imtest Route 에러 해결
    2022-06-29

--- src/Questions/Question.tsx
+++ src/Questions/Question.tsx
@@ -13,14 +13,38 @@ const Question = () => {
 
   const [bingo, setBingo] = useRecoilState(bingoAtom);
   const correctAnswer = DATA[Number(id) - 1].answer;
+  // const a = DATA.filter(Number(id) === , Number(id))
   const [answer, setAnswer] = useState(-1);
+
+  // 지금 프로그램 에러 이유..
+  // 온클릭에서 네이게이트를 줘서
+  // answer값이 바뀌고 그 값과 정답을 비교해서
+  // 정답이면 1점 더해주는 연산을 하기 전에
+  // 다음 페이지로 넘겨버려서 정답이 꼬임
+
+  // useEffect에 네이게이트를 넣으면
+  // -1로 초기화 해줄 때도 네비게이트가 되어서
+  // 한 번에 2페이지씩 넘어감
+
+  // -1로 초기화 하지 않으면
+  // 그 전 문제의 답을 1로 체크했으면
+  // 다음문제에선 1로 체크하면 같은 값이여서
+  // 변했다고 인정안하고 다음 페이지로 넘어가지 않음
 
   useEffect(() => {
     if (correctAnswer === answer) {
       setBingo((prev) => prev + 1);
     }
+    setCnt((prev) => prev + 1);
     setAnswer(-1);
   }, [answer]);
+
+  const [cnt, setCnt] = useState(0);
+  useEffect(() => {
+    if (cnt % 2 == 1) {
+      navigate(Number(id) === 12 ? "/end" : `/questions/${Number(id) + 1}`);
+    }
+  }, [cnt]);
 
   const validQuestionData = DATA[Number(id) - 1];
   return (
@@ -31,9 +55,6 @@ const Question = () => {
           <LeftCategory
             onClick={() => {
               setAnswer(0);
-              navigate(
-                Number(id) + 1 === 12 ? "/end" : `/questions/${Number(id) + 1}`
-              );
             }}
           >
             <ImgWrapper src={validQuestionData.images[0]} alt="왼쪽 사진" />
@@ -41,9 +62,6 @@ const Question = () => {
           <RightCategory
             onClick={() => {
               setAnswer(1);
-              navigate(
-                Number(id) + 1 === 12 ? "/end" : `/questions/${Number(id) + 1}`
-              );
             }}
           >
             <ImgWrapper src={validQuestionData.images[1]} alt="오른쪽 사진" />
